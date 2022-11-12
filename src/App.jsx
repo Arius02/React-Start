3f693170aa9bc5706e2c37938b2ba996c07df384
Arius02 <90223545+Arius02@users.noreply.github.com>
2022-11-13 00:00:47 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Route, Routes , BrowserRouter } from 'react-router-dom';
+import './App.css';
+
+import  About  from './Components/About/About';
+import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
+import Portfolio from './Components/Portfolio/Portfolio';
+
+function App() {
+  return (
+  <>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route index element={<Home/>}/>
+        <Route path='/portfolio' element={<Portfolio/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+  </>
+  );
+}
+
+export default App;
